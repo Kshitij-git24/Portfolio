928240e70b576b8dad80cd4380ec39f75a018b1a
Kshitij-git24 <kshitijchaudhari12345@gmail.com>
2024-01-04 11:54:52 -0500

    package lock file

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -2,11 +2,13 @@ import "../components/Navbar.scss";
 
 export const Navbar = () => {
   return (
-    <div>
-      <a href="#">Home</a>
-      <a href="#">Tech and Skills</a>
-      <a href="#">Projects</a>
-      <a href="#">Contact</a>
+    <div className="navbar">
+      <div className="wrapper">
+        <a href="#">Home</a>
+        <a href="#">Tech and Skills</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </div>
     </div>
   );
 };
