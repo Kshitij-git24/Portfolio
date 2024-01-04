b2c889189ea5dbeedca5c7c8d56c61a9b6b1f12f
Kshitij-git24 <kshitijchaudhari12345@gmail.com>
2024-01-03 22:12:02 -0500

    Content layout and Navbar

---
+++ portfolio/src/components/Content.js
@@ -0,0 +1,11 @@
+import "../components/Content.scss";
+export const Content = () => {
+  return (
+    <div>
+      <section>Home</section>
+      <section>Projects</section>
+      <section>Tech and Skills</section>
+      <section>Contact</section>
+    </div>
+  );
+};
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import "../components/Navbar.scss";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">Tech and Skills</a>
+      <a href="#">Projects</a>
+      <a href="#">Contact</a>
+    </div>
+  );
+};
