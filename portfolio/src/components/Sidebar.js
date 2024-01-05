e4bc5834f70073846ae486cdc2791837b2d08739
Kshitij-git24 <kshitijchaudhari12345@gmail.com>
2024-01-05 00:30:49 -0500

    Navbar complete

---
+++ portfolio/src/components/Sidebar.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "../components/Navbar.scss";
+import { motion } from "framer-motion";
+import "../components/Sidebar.scss";
+import closeMenuImage from "../images/closeMenu.png";
+import menuImage from "../images/menu.png";
+import Links from "./Links";
+
+export const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+
+  const variants = {
+    open: {
+      clipPath: "circle(1200px at 50px 50px)",
+      transition: {
+        type: "spring",
+        stiffness: 20,
+      },
+    },
+    closed: {
+      clipPath: "circle(30px at 50px 50px)",
+      transition: {
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
+  return (
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <motion.div className="links">
+          <Links />
+        </motion.div>
+        <button onClick={() => setOpen(!open)}>
+          <img
+            className="menuImg"
+            src={open ? closeMenuImage : menuImage}
+            alt={open ? "close" : "menu"}
+          />
+        </button>
+      </motion.div>
+    </motion.div>
+  );
+};
